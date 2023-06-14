51756ba37004328f0da5c88de315b69364ae74ec
Pavel Kirillov <work.pavelek@mail.ru>
2023-06-14 12:54:36 +0300

    added Client

---
+++ Chat/mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "chatwindow.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_enterButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    ChatWindow *chatwindow;
+};
+
+#endif // MAINWINDOW_H
